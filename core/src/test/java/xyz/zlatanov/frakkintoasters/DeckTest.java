b7e9e4a7ef46489943e99f7bfc12e886a5acdc0e
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-03-27 15:48:25 +0000

    Add deck of cards support

--- core/src/test/java/xyz/zlatanov/frakkintoasters/DeckTest.java
+++ core/src/test/java/xyz/zlatanov/frakkintoasters/DeckTest.java
@@ -1,5 +1,21 @@
 package xyz.zlatanov.frakkintoasters;
 
+import org.junit.jupiter.api.Test;
+
 class DeckTest {
 
+    @Test
+    void shouldDrawCardFromTopOfDeck(){
+
+    }
+
+    @Test
+    void shouldDiscardCard(){
+        //todo discard card from outside of deck and keep it for future shuffling "next" to deck
+    }
+
+    @Test
+    void shouldShuffleDeck(){
+        //todo add discarded cards and shuffle deck
+    }
 }
