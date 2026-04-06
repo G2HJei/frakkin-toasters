41887ffbb2186ea58f24ef9c8caf59d8f6ba21e8
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-04-06 09:48:53 +0100

    Refactor Location

--- core/src/main/java/xyz/zlatanov/frakkintoasters/state/board/Location.java
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/state/board/Location.java
@@ -2,7 +2,6 @@ package xyz.zlatanov.frakkintoasters.state.board;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public enum Location {
     //galactica
@@ -69,16 +68,13 @@ public enum Location {
             "Galactica", List.of(FTL_CONTROL, WEAPONS_CONTROL, COMMUNICATIONS, RESEARCH_LAB, ARMORY, COMMAND, ADMIRALS_QUARTERS, HANGAR_DECK, SICKBAY, BRIG),
             "Galactica space", List.of(GALACTICA_SPACE_12_OCLOCK, GALACTICA_SPACE_2_OCLOCK, GALACTICA_SPACE_4_OCLOCK, GALACTICA_SPACE_6_OCLOCK, GALACTICA_SPACE_8_OCLOCK, GALACTICA_SPACE_10_OCLOCK),
             "Colonial One", List.of(PRESS_ROOM, PRESIDENTS_OFFICE, ADMINISTRATION),
-            "Cylon Locations", List.of(CAPRICA, CYLON_FLEET, HUMAN_FLEET, RESURRECTION_SHIP, HUB_DESTROYED),
+            "Cylon locations & fleet", List.of(BASESTAR_BRIDGE, CAPRICA, CYLON_FLEET, HUMAN_FLEET, RESURRECTION_SHIP, HUB_DESTROYED),
+            "Cylon fleet space", List.of(CYLON_FLEET_SPACE_1, CYLON_FLEET_SPACE_2, CYLON_FLEET_SPACE_3, CYLON_FLEET_SPACE_4, CYLON_FLEET_SPACE_5_6, CYLON_FLEET_SPACE_7_8),
             "Pegasus", List.of(PEGASUS_CIC, AIRLOCK, MAIN_BATTERIES, ENGINE_ROOM),
-            "Demetrius", List.of(BRIDGE, TACTICAL_PLOT, CAPTAINS_CABIN),
-            "Cylon fleet", List.of(BASESTAR_BRIDGE)
+            "Demetrius", List.of(BRIDGE, TACTICAL_PLOT, CAPTAINS_CABIN)
     );
-    private static final Set<Location>               spaceLocations     = Set.of(
-            GALACTICA_SPACE_12_OCLOCK, GALACTICA_SPACE_2_OCLOCK, GALACTICA_SPACE_4_OCLOCK, GALACTICA_SPACE_6_OCLOCK, GALACTICA_SPACE_8_OCLOCK, GALACTICA_SPACE_10_OCLOCK);
-    private static final Set<Location>               hazardousLocations = Set.of(
-            SICKBAY, BRIG, RESURRECTION_SHIP
-    );
+    private static final List<Location>              spaceLocations     = LOCATION_AREAS.get("Galactica space");
+    private static final List<Location>              hazardousLocations = List.of(SICKBAY, BRIG, RESURRECTION_SHIP, HUB_DESTROYED);
 
     public boolean isSpaceLocation() {
         return spaceLocations.contains(this);
