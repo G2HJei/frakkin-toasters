7d00db1f60b3306c76274efffb434b448cc578be
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-04-12 06:57:18 +0100

    Prepare tests in DamageBasestarEventTest

--- core/src/main/java/xyz/zlatanov/frakkintoasters/event/DamageBasestarEvent.java
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/event/DamageBasestarEvent.java
@@ -1,16 +1,4 @@
 package xyz.zlatanov.frakkintoasters.event;
 
-import xyz.zlatanov.frakkintoasters.state.Game;
-import xyz.zlatanov.frakkintoasters.state.board.Location;
-import xyz.zlatanov.frakkintoasters.state.damage.BasestarDamage;
-import xyz.zlatanov.frakkintoasters.state.ship.Basestar;
-
-import java.util.List;
-
 public record DamageBasestarEvent() implements Event {
-    @Override
-    public List<Followup> apply(Game game) {
-        ((Basestar) game.boards().galactica().shipsIn(Location.GALACTICA_SPACE_2_OCLOCK).getFirst()).damage().add(BasestarDamage.CRITICAL_HIT);
-        return List.of();
-    }
 }
