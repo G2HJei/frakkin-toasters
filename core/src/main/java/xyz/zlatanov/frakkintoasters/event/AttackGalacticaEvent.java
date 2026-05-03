6d92a48ff25e0ef34d7f9dd8fc6f532b59a8d6f2
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-05-03 13:31:17 +0100

    Refactor ActivateRaidersEventTest

---
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/event/AttackGalacticaEvent.java
@@ -0,0 +1,4 @@
+package xyz.zlatanov.frakkintoasters.event;
+
+public record AttackGalacticaEvent() implements Event {
+}
--- core/src/test/java/xyz/zlatanov/frakkintoasters/event/ActivateRaidersEventTest.java
+++ core/src/test/java/xyz/zlatanov/frakkintoasters/event/ActivateRaidersEventTest.java
@@ -3,8 +3,11 @@ package xyz.zlatanov.frakkintoasters.event;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import xyz.zlatanov.frakkintoasters.state.Game;
+import xyz.zlatanov.frakkintoasters.state.board.GalacticaBoard;
 import xyz.zlatanov.frakkintoasters.state.ship.CivilianShip;
 import xyz.zlatanov.frakkintoasters.state.ship.Viper;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static xyz.zlatanov.frakkintoasters.event.Followup.*;
@@ -15,204 +18,191 @@ import static xyz.zlatanov.frakkintoasters.state.ship.ShipType.RAIDER;
 
 class ActivateRaidersEventTest {
 
+    Game           game           = Game.builder().build();
+    GalacticaBoard galacticaBoard = game.boards().galactica();
+
+
     @Test
     void shouldPlaceRaiderOnCylonFleetBoardWhenNoRaidersOrBasestarsOnMainBoard() {
-        val game = Game.builder().build();
-        // no raiders, no basestars on main board
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
         assertEquals(
                 all(new PlaceShipOnCylonFleetBoardEvent(RAIDER), new AdvancePursuitTrackEvent()),
                 followup);
     }
 
-    // === Rule 5: Launch ===
 
     @Test
     void shouldLaunchTwoRaidersFromEachBasestarWhenNoRaidersOnBoard() {
-        val game = Game.builder().build();
         val basestar = game.cylonShips().basestar();
-        game.boards().galactica().place(GALACTICA_SPACE_8_OCLOCK, basestar);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
-        val raiders = game.boards().galactica().shipsIn(GALACTICA_SPACE_8_OCLOCK, RAIDER);
+        val raiders = galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, RAIDER);
         assertEquals(2, raiders.size());
         assertEquals(NONE, followup);
     }
 
     @Test
     void shouldLaunchTwoRaidersFromEachOfMultipleBasestars() {
-        val game = Game.builder().build();
         val basestar1 = game.cylonShips().basestar();
         val basestar2 = game.cylonShips().basestar();
-        game.boards().galactica().place(GALACTICA_SPACE_8_OCLOCK, basestar1);
-        game.boards().galactica().place(GALACTICA_SPACE_2_OCLOCK, basestar2);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar1);
+        galacticaBoard.place(GALACTICA_SPACE_2_OCLOCK, basestar2);
 
-        new ActivateRaidersEvent().execute(game);
+        executeEvent();
 
-        assertEquals(2, game.boards().galactica().shipsIn(GALACTICA_SPACE_8_OCLOCK, RAIDER).size());
-        assertEquals(2, game.boards().galactica().shipsIn(GALACTICA_SPACE_2_OCLOCK, RAIDER).size());
+        assertEquals(2, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, RAIDER).size());
+        assertEquals(2, galacticaBoard.shipsIn(GALACTICA_SPACE_2_OCLOCK, RAIDER).size());
     }
 
-    // === Rule 4: Attack Galactica ===
 
     @Test
     void shouldAttackGalacticaWhenNoCivilianShipsOnBoard() {
         val game = Game.builder().build();
         val raider = game.cylonShips().raider();
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        // no civilians anywhere
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
 
         val followup = new ActivateRaidersEvent().execute(game);
 
+        //todo change to AttackGalacticaEvent
         assertEquals(single(new DamageGalacticaEvent()), followup);
     }
 
+    //todo wrong test, correct it and then fix behavior!
     @Test
     void shouldAttackGalacticaOnceEvenWithMultipleRaiders() {
         val game = Game.builder().build();
         val raider1 = game.cylonShips().raider();
         val raider2 = game.cylonShips().raider();
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider1);
-        game.boards().galactica().place(GALACTICA_SPACE_8_OCLOCK, raider2);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider1);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, raider2);
 
         val followup = new ActivateRaidersEvent().execute(game);
 
-        // each raider attacks galactica
+        // TODO each raider attacks galactica. Change this to AttackGalacticaEvents
         assertEquals(all(new DamageGalacticaEvent(), new DamageGalacticaEvent()), followup);
     }
 
-    // === Rule 3: Move toward nearest civilian ship ===
+    //todo add test when one raider attacks galactica, other attacks viper, third attacks manned viper, fourth attacks civ ship
+    //todo add test when one raider destroys civ ship, then the other no longer has target and moves. This should actually refactor the whole event into activate raiders with id of specific raider being activated
 
     @Test
     void shouldMoveRaiderTowardNearestCivilianShip() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val civilian = new CivilianShip(500, 0, 0, 1);
-        game.boards().galactica().place(GALACTICA_SPACE_12_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian);
+        galacticaBoard.place(GALACTICA_SPACE_12_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, civilian);
 
-        new ActivateRaidersEvent().execute(game);
+        executeEvent();
 
-        assertEquals(GALACTICA_SPACE_2_OCLOCK, game.boards().galactica().locate(raider));
+        assertEquals(GALACTICA_SPACE_2_OCLOCK, galacticaBoard.locate(raider));
     }
 
     @Test
     void shouldMoveClockwiseWhenEquidistantCivilianShips() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val civilian1 = new CivilianShip(500, 0, 0, 1);
         val civilian2 = new CivilianShip(501, 0, 0, 1);
-        game.boards().galactica().place(GALACTICA_SPACE_12_OCLOCK, raider);
-        // equidistant: 4 o'clock (2 steps clockwise) and 8 o'clock (2 steps counter-clockwise)
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian1);
-        game.boards().galactica().place(GALACTICA_SPACE_8_OCLOCK, civilian2);
+        galacticaBoard.place(GALACTICA_SPACE_12_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_2_OCLOCK, civilian1);
+        galacticaBoard.place(GALACTICA_SPACE_10_OCLOCK, civilian2);
 
-        new ActivateRaidersEvent().execute(game);
+        executeEvent();
 
-        // should move clockwise toward 4 o'clock
-        assertEquals(GALACTICA_SPACE_2_OCLOCK, game.boards().galactica().locate(raider));
+        assertEquals(GALACTICA_SPACE_2_OCLOCK, galacticaBoard.locate(raider));
     }
 
     @Test
     void shouldMoveRaiderTowardNearestCivilianCounterClockwise() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val civilian = new CivilianShip(500, 0, 0, 1);
-        game.boards().galactica().place(GALACTICA_SPACE_12_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_10_OCLOCK, civilian);
+        galacticaBoard.place(GALACTICA_SPACE_12_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_10_OCLOCK, civilian);
 
-        new ActivateRaidersEvent().execute(game);
+        executeEvent();
 
-        assertEquals(GALACTICA_SPACE_10_OCLOCK, game.boards().galactica().locate(raider));
+        assertEquals(GALACTICA_SPACE_10_OCLOCK, galacticaBoard.locate(raider));
     }
 
     @Test
     void shouldNotMoveRaiderIfCivilianShipInSameArea() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val civilian = new CivilianShip(500, 0, 0, 1);
-        // raider and civilian in same area - raider should destroy civilian (rule 2), not move
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, civilian);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        executeEvent();
 
-        // should destroy civilian, not move
-        assertEquals(GALACTICA_SPACE_4_OCLOCK, game.boards().galactica().locate(raider));
+        assertEquals(GALACTICA_SPACE_4_OCLOCK, galacticaBoard.locate(raider));
     }
 
-    // === Rule 2: Destroy Civilian Ship ===
 
     @Test
     void shouldDestroyCivilianShipWhenNoVipersInArea() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val civilian = new CivilianShip(500, 0, 0, 1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, civilian);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
         assertEquals(single(new DestroyCivilianShipEvent(500)), followup);
     }
 
     @Test
     void shouldLetPlayerChooseWhenMultipleCivilianShipsInArea() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val civilian1 = new CivilianShip(500, 0, 0, 1);
         val civilian2 = new CivilianShip(501, 0, 1, 0);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian2);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, civilian1);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, civilian2);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
         assertEquals(one(new DestroyCivilianShipEvent(500), new DestroyCivilianShipEvent(501)), followup);
     }
 
-    // === Rule 1: Attack a Viper ===
 
+    // === Rule 1: Attack a Viper ===
     @Test
     void shouldAttackUnmannedViperFirst() {
         val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val unmannedViper = new Viper(500);
         val pilotedViper = new Viper(501).pilot(KARA_STARBUCK_THRACE);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, unmannedViper);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, pilotedViper);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, unmannedViper);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, pilotedViper);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
         assertEquals(single(new DamageVipersEvent(java.util.Set.of(500))), followup);
     }
 
     @Test
     void shouldAttackPilotedViperWhenNoUnmannedVipers() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val pilotedViper = new Viper(500).pilot(KARA_STARBUCK_THRACE);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, pilotedViper);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, pilotedViper);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
         assertEquals(single(new DamageVipersEvent(java.util.Set.of(500))), followup);
     }
 
     @Test
     void shouldLetPlayerChooseBetweenTwoPilotedVipers() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val pilotedViper1 = new Viper(500).pilot(KARA_STARBUCK_THRACE);
         val pilotedViper2 = new Viper(501).pilot(LEE_APOLLO_ADAMA);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, pilotedViper1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, pilotedViper2);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, pilotedViper1);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, pilotedViper2);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
         assertEquals(
                 one(new DamageVipersEvent(java.util.Set.of(500)), new DamageVipersEvent(java.util.Set.of(501))),
@@ -221,55 +211,53 @@ class ActivateRaidersEventTest {
 
     @Test
     void shouldPreferViperOverCivilianShip() {
-        val game = Game.builder().build();
         val raider = game.cylonShips().raider();
         val viper = new Viper(500);
         val civilian = new CivilianShip(501, 0, 0, 1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, viper);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, civilian);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, viper);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, civilian);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
-        // attack viper, not destroy civilian
         assertEquals(single(new DamageVipersEvent(java.util.Set.of(500))), followup);
     }
 
-    // === Multiple raiders in different areas ===
 
     @Test
     void shouldHandleMultipleRaidersInDifferentAreas() {
-        val game = Game.builder().build();
         val raider1 = game.cylonShips().raider();
         val raider2 = game.cylonShips().raider();
         val viper = new Viper(500);
         val civilian = new CivilianShip(501, 0, 0, 1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, viper);
-        game.boards().galactica().place(GALACTICA_SPACE_8_OCLOCK, raider2);
-        game.boards().galactica().place(GALACTICA_SPACE_8_OCLOCK, civilian);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider1);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, viper);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, raider2);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, civilian);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
-        // raider1 attacks viper, raider2 destroys civilian
         assertEquals(
-                all(new DamageVipersEvent(java.util.Set.of(500)), new DestroyCivilianShipEvent(501)),
+                //todo should be attack viper not damage, and destroy should be separate test
+                all(new DamageVipersEvent(Set.of(500)), new DestroyCivilianShipEvent(501)),
                 followup);
     }
 
     @Test
     void shouldHandleMultipleRaidersAttackingSameUnmannedViper() {
-        val game = Game.builder().build();
         val raider1 = game.cylonShips().raider();
         val raider2 = game.cylonShips().raider();
         val viper = new Viper(500);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider1);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, raider2);
-        game.boards().galactica().place(GALACTICA_SPACE_4_OCLOCK, viper);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider1);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, raider2);
+        galacticaBoard.place(GALACTICA_SPACE_4_OCLOCK, viper);
 
-        val followup = new ActivateRaidersEvent().execute(game);
+        val followup = executeEvent();
 
-        // both raiders attack the same unmanned viper
         assertEquals(all(new DamageVipersEvent(java.util.Set.of(500)), new DamageVipersEvent(java.util.Set.of(500))), followup);
+    }
+
+    private Followup executeEvent() {
+        return new ActivateRaidersEvent().execute(game);
     }
 }
