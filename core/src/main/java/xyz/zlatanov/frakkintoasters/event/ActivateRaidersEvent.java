b147a0c1ecd25db0f23090abdc6f33bb313a5d5f
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-05-03 15:07:56 +0100

    Refactor ActivateRaidersEvent. Prepare to move single raider activation logic in ActivateRaiderEvent

--- core/src/main/java/xyz/zlatanov/frakkintoasters/event/ActivateRaidersEvent.java
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/event/ActivateRaidersEvent.java
@@ -10,34 +10,45 @@ import xyz.zlatanov.frakkintoasters.state.ship.Raider;
 import java.util.Comparator;
 import java.util.List;
 
-import static xyz.zlatanov.frakkintoasters.event.Followup.all;
 import static xyz.zlatanov.frakkintoasters.state.board.Location.LOCATION_AREAS;
 import static xyz.zlatanov.frakkintoasters.state.ship.ShipType.RAIDER;
 
 public record ActivateRaidersEvent() implements Event {
 
+    //todo replace record with class so private variables can be used to improve readability (maybe?)
+
     public static final List<Location> SPACE_AREAS_CLOCKWISE = LOCATION_AREAS.get("Galactica space");
 
     @Override
     public Followup apply(Game game) {
-        val galactica = game.boards().galactica();
-        val raiders = galactica.shipsInSpace(Raider.class);
-        val basestars = galactica.shipsInSpace(Basestar.class);
+        val galacticaBoard = game.boards().galactica();
+        val raiders = getRaiders(galacticaBoard);
+        val basestars = galacticaBoard.shipsInSpace(Basestar.class);
 
-        if (raiders.isEmpty() && basestars.isEmpty()) {
-            return all(new PlaceShipOnCylonFleetBoardEvent(RAIDER), new AdvancePursuitTrackEvent());
+        if (!raiders.isEmpty()) {
+            return activateRaidersOneByOne(raiders, galacticaBoard);
+        } else if (basestars.isEmpty()) {
+            return placeOnCylonFleetBoard();
+        } else {
+            return launchRaiders(game, basestars);
         }
-
-        if (raiders.isEmpty()) {
-            launchRaiders(game, basestars);
-            return Followup.NONE;
-        }
-
-        val raiderFollowups = buildRaiderActivationEvents(raiders, galactica);
-        return Followup.all(raiderFollowups);
     }
 
-    private void launchRaiders(Game game, List<Basestar> basestars) {
+    private List<Raider> getRaiders(GalacticaBoard galactica) {
+        return galactica.shipsInSpace(Raider.class)
+                .stream()
+                .sorted(Comparator.comparingInt(a -> SPACE_AREAS_CLOCKWISE.indexOf(galactica.locate(a))))
+                .toList();
+    }
+
+    private Followup activateRaidersOneByOne(List<Raider> raiders, GalacticaBoard galactica) {
+        return Followup.all(
+                raiders.stream()
+                        .map(r -> new ActivateRaiderEvent(r.id()))
+                        .toArray(Event[]::new));
+    }
+
+    private Followup launchRaiders(Game game, List<Basestar> basestars) {
         val galactica = game.boards().galactica();
         for (val basestar : basestars) {
             val basestarLocation = galactica.locate(basestar);
@@ -48,12 +59,10 @@ public record ActivateRaidersEvent() implements Event {
                 galactica.place(basestarLocation, game.cylonShips().raider());
             }
         }
+        return Followup.NONE;
     }
 
-    private static Event[] buildRaiderActivationEvents(List<Raider> raiders, GalacticaBoard galactica) {
-        return raiders.stream()
-                .sorted(Comparator.comparingInt(a -> SPACE_AREAS_CLOCKWISE.indexOf(galactica.locate(a))))
-                .map(r -> new ActivateRaiderEvent(r.id()))
-                .toArray(Event[]::new);
+    private Followup placeOnCylonFleetBoard() {
+        return Followup.all(new PlaceShipOnCylonFleetBoardEvent(RAIDER), new AdvancePursuitTrackEvent());
     }
 }
