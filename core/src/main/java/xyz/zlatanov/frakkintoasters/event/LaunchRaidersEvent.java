de80411a4eaa8ed45eef35e3e975e22715825716
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-05-07 00:06:34 +0100

    Implement LaunchRaidersEvent

---
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/event/LaunchRaidersEvent.java
@@ -0,0 +1,49 @@
+package xyz.zlatanov.frakkintoasters.event;
+
+import lombok.val;
+import xyz.zlatanov.frakkintoasters.state.Game;
+import xyz.zlatanov.frakkintoasters.state.ship.Basestar;
+
+import java.util.List;
+
+import static xyz.zlatanov.frakkintoasters.event.Followup.NONE;
+import static xyz.zlatanov.frakkintoasters.event.Followup.all;
+import static xyz.zlatanov.frakkintoasters.state.damage.BasestarDamage.DISABLED_HANGAR_BAY;
+import static xyz.zlatanov.frakkintoasters.state.ship.ShipType.BASESTAR;
+
+public record LaunchRaidersEvent() implements Event {
+
+    @Override
+    public Followup apply(Game game) {
+        val basestars = game.boards().galactica().shipsInSpace(Basestar.class);
+        if (basestars.isEmpty()) {
+            return placeBasestarOnCylonFleetBoard();
+        } else {
+            return launchRaidersFromEachBasestar(game, basestars);
+        }
+    }
+
+    private Followup placeBasestarOnCylonFleetBoard() {
+        return all(new PlaceShipOnCylonFleetBoardEvent(BASESTAR), new AdvancePursuitTrackEvent());
+    }
+
+    private Followup launchRaidersFromEachBasestar(Game game, List<Basestar> basestars) {
+        basestars.stream()
+                .filter(b -> !b.damage().contains(DISABLED_HANGAR_BAY))
+                .forEach(b -> launch3Raiders(game, b));
+        return NONE;
+    }
+
+    private void launch3Raiders(Game game, Basestar basestar) {
+        val galacticaBoard = game.boards().galactica();
+        val basestarLocation = galacticaBoard.locate(basestar);
+        int raidersLaunched = 0;
+        while (raidersLaunched++ < 3 && raidersAvailable(game)) {
+            galacticaBoard.place(basestarLocation, game.cylonShips().raider());
+        }
+    }
+
+    private boolean raidersAvailable(Game game) {
+        return !game.cylonShips().raiders().isEmpty();
+    }
+}
---
+++ core/src/test/java/xyz/zlatanov/frakkintoasters/event/LaunchRaidersEventTest.java
@@ -0,0 +1,115 @@
+package xyz.zlatanov.frakkintoasters.event;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import xyz.zlatanov.frakkintoasters.state.Game;
+import xyz.zlatanov.frakkintoasters.state.board.GalacticaBoard;
+import xyz.zlatanov.frakkintoasters.state.ship.CylonShips;
+import xyz.zlatanov.frakkintoasters.state.ship.Raider;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static xyz.zlatanov.frakkintoasters.event.Followup.NONE;
+import static xyz.zlatanov.frakkintoasters.event.Followup.all;
+import static xyz.zlatanov.frakkintoasters.state.board.Location.GALACTICA_SPACE_2_OCLOCK;
+import static xyz.zlatanov.frakkintoasters.state.board.Location.GALACTICA_SPACE_8_OCLOCK;
+import static xyz.zlatanov.frakkintoasters.state.damage.BasestarDamage.DISABLED_HANGAR_BAY;
+import static xyz.zlatanov.frakkintoasters.state.damage.BasestarDamage.STRUCTURAL_DAMAGE;
+import static xyz.zlatanov.frakkintoasters.state.ship.ShipType.BASESTAR;
+
+class LaunchRaidersEventTest {
+
+    Game           game           = Game.builder().build();
+    GalacticaBoard galacticaBoard = game.boards().galactica();
+
+    @Test
+    void shouldPlaceBasestarOnCylonFleetBoardWhenNoBasestarsOnMainBoard() {
+        val followup = executeEvent();
+
+        assertEquals(
+                all(new PlaceShipOnCylonFleetBoardEvent(BASESTAR), new AdvancePursuitTrackEvent()),
+                followup);
+    }
+
+    @Test
+    void shouldLaunchThreeRaidersFromBasestar() {
+        val basestar = game.cylonShips().basestar();
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar);
+
+        val followup = executeEvent();
+
+        assertEquals(3, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, Raider.class).size());
+        assertEquals(NONE, followup);
+    }
+
+    @Test
+    void shouldLaunchThreeRaidersFromEachOfMultipleBasestars() {
+        val basestar1 = game.cylonShips().basestar();
+        val basestar2 = game.cylonShips().basestar();
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar1);
+        galacticaBoard.place(GALACTICA_SPACE_2_OCLOCK, basestar2);
+
+        val followup = executeEvent();
+
+        assertEquals(3, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, Raider.class).size());
+        assertEquals(3, galacticaBoard.shipsIn(GALACTICA_SPACE_2_OCLOCK, Raider.class).size());
+        assertEquals(NONE, followup);
+    }
+
+    @Test
+    void shouldStopLaunchingWhenRaiderSupplyIsExhausted() {
+        val game = Game.builder()
+                .cylonShips(CylonShips.builder().raiders(2).build())
+                .build();
+        val galacticaBoard = game.boards().galactica();
+        val basestar = game.cylonShips().basestar();
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar);
+
+        val followup = new LaunchRaidersEvent().execute(game);
+
+        assertEquals(2, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, Raider.class).size());
+        assertEquals(NONE, followup);
+    }
+
+    @Test
+    void shouldDoNothingWhenOnlyBasestarHasDisabledHangarBay() {
+        val basestar = game.cylonShips().basestar();
+        basestar.damage(DISABLED_HANGAR_BAY);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar);
+
+        val followup = executeEvent();
+
+        assertEquals(0, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, Raider.class).size());
+        assertEquals(NONE, followup);
+    }
+
+    @Test
+    void shouldLaunchFromHealthyBasestarButNotFromHangarDisabledOne() {
+        val healthy = game.cylonShips().basestar();
+        val disabled = game.cylonShips().basestar();
+        disabled.damage(DISABLED_HANGAR_BAY);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, healthy);
+        galacticaBoard.place(GALACTICA_SPACE_2_OCLOCK, disabled);
+
+        val followup = executeEvent();
+
+        assertEquals(3, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, Raider.class).size());
+        assertEquals(0, galacticaBoard.shipsIn(GALACTICA_SPACE_2_OCLOCK, Raider.class).size());
+        assertEquals(NONE, followup);
+    }
+
+    @Test
+    void shouldLaunchRaidersWhenBasestarHasOtherDamageButHangarIsFunctional() {
+        val basestar = game.cylonShips().basestar();
+        basestar.damage(STRUCTURAL_DAMAGE);
+        galacticaBoard.place(GALACTICA_SPACE_8_OCLOCK, basestar);
+
+        val followup = executeEvent();
+
+        assertEquals(3, galacticaBoard.shipsIn(GALACTICA_SPACE_8_OCLOCK, Raider.class).size());
+        assertEquals(NONE, followup);
+    }
+
+    Followup executeEvent() {
+        return new LaunchRaidersEvent().execute(game);
+    }
+}
