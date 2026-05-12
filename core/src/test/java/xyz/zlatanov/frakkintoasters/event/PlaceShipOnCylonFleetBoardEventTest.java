c328bd9b0793563d724a3d31ad9022877eb9ef92
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-05-12 11:10:02 +0100

    Implement PlaceShipOnCylonFleetBoardEvent

---
+++ core/src/test/java/xyz/zlatanov/frakkintoasters/event/PlaceShipOnCylonFleetBoardEventTest.java
@@ -0,0 +1,58 @@
+package xyz.zlatanov.frakkintoasters.event;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import xyz.zlatanov.frakkintoasters.fake.FakeDie;
+import xyz.zlatanov.frakkintoasters.state.Game;
+import xyz.zlatanov.frakkintoasters.state.board.Location;
+import xyz.zlatanov.frakkintoasters.state.exception.FrakCallTheAdmiralException;
+import xyz.zlatanov.frakkintoasters.state.ship.*;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static xyz.zlatanov.frakkintoasters.state.board.Location.*;
+import static xyz.zlatanov.frakkintoasters.state.ship.ShipType.*;
+
+class PlaceShipOnCylonFleetBoardEventTest {
+
+    FakeDie fakeDie = new FakeDie();
+    Game    game    = Game.builder().die(fakeDie).build();
+
+    @ParameterizedTest
+    @MethodSource("shouldPlaceShipOnCorrespondingSpaceAreaArgs")
+    void shouldPlaceShipOnCorrespondingSpaceArea(Class<Ship> shipClass, int nextDieRoll, Location placementLocation) {
+        fakeDie.nextRoll(nextDieRoll);
+        executeEvent(shipTypeFor(shipClass));
+        assertEquals(1, game.boards().cylonFleet().shipsIn(placementLocation, shipClass).size());
+    }
+
+    static Stream<Arguments> shouldPlaceShipOnCorrespondingSpaceAreaArgs() {
+        return Stream.of(
+                Arguments.arguments(Basestar.class, 1, CYLON_FLEET_SPACE_1),
+                Arguments.arguments(Basestar.class, 2, CYLON_FLEET_SPACE_2),
+                Arguments.arguments(Basestar.class, 3, CYLON_FLEET_SPACE_3),
+                Arguments.arguments(Basestar.class, 4, CYLON_FLEET_SPACE_4),
+                Arguments.arguments(Basestar.class, 5, CYLON_FLEET_SPACE_5_6),
+                Arguments.arguments(Basestar.class, 6, CYLON_FLEET_SPACE_5_6),
+                Arguments.arguments(Raider.class, 7, CYLON_FLEET_SPACE_7_8),
+                Arguments.arguments(HeavyRaider.class, 8, CYLON_FLEET_SPACE_7_8)
+        );
+    }
+
+    void executeEvent(ShipType shipType) {
+        new PlaceShipOnCylonFleetBoardEvent(shipType).execute(game);
+    }
+
+    <T extends Ship> ShipType shipTypeFor(Class<T> shipClass) {
+        if (shipClass.equals(Raider.class)) {
+            return RAIDER;
+        } else if (shipClass.equals(HeavyRaider.class)) {
+            return HEAVY_RAIDER;
+        } else if (shipClass.equals(Basestar.class)) {
+            return BASESTAR;
+        }
+        throw new FrakCallTheAdmiralException();
+    }
+}
