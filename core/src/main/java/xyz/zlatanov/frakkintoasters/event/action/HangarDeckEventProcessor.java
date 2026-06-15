a1f359db6eeb571441bcce472ab29a5d3a9c7fc1
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-06-15 16:24:37 +0100

    Expand Hangar locaiton event

--- core/src/main/java/xyz/zlatanov/frakkintoasters/event/action/HangarDeckEventProcessor.java
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/event/action/HangarDeckEventProcessor.java
@@ -3,18 +3,32 @@ package xyz.zlatanov.frakkintoasters.event.action;
 import xyz.zlatanov.frakkintoasters.EventProcessor;
 import xyz.zlatanov.frakkintoasters.event.ActionEvent;
 import xyz.zlatanov.frakkintoasters.event.Followup;
+import xyz.zlatanov.frakkintoasters.event.NoOpEvent;
 import xyz.zlatanov.frakkintoasters.event.placeholder.PlayerDecisionEvent;
 import xyz.zlatanov.frakkintoasters.event.player.LaunchViperEvent;
+import xyz.zlatanov.frakkintoasters.state.skill.SkillSetOption;
 
-import static xyz.zlatanov.frakkintoasters.event.Followup.all;
+import static xyz.zlatanov.frakkintoasters.event.Followup.*;
+import static xyz.zlatanov.frakkintoasters.state.skill.SkillCardColor.PILOTING;
 
 public class HangarDeckEventProcessor extends EventProcessor<HangarDeckEvent> {
 
     @Override
+    protected boolean isValid() {
+        return player.character().skillSet()
+                .stream()
+                .map(SkillSetOption::colors)
+                .anyMatch(colors -> colors.contains(PILOTING));
+    }
+
+    @Override
     public Followup process() {
         return all(
-                new PlayerDecisionEvent<>(player.number(), LaunchViperEvent.class),
-                new PlayerDecisionEvent<>(player.number(), ActionEvent.class));
+                single(
+                        new PlayerDecisionEvent<>(player.number(), LaunchViperEvent.class)),
+                one(
+                        new PlayerDecisionEvent<>(player.number(), ActionEvent.class),
+                        new NoOpEvent(player.number())));
     }
 
 }
--- core/src/test/java/xyz/zlatanov/frakkintoasters/event/action/HangarDeckEventProcessorTest.java
+++ core/src/test/java/xyz/zlatanov/frakkintoasters/event/action/HangarDeckEventProcessorTest.java
@@ -1,30 +1,37 @@
 package xyz.zlatanov.frakkintoasters.event.action;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import xyz.zlatanov.frakkintoasters.event.ActionEvent;
 import xyz.zlatanov.frakkintoasters.event.EventTestHarness;
+import xyz.zlatanov.frakkintoasters.event.NoOpEvent;
 import xyz.zlatanov.frakkintoasters.event.placeholder.PlayerDecisionEvent;
 import xyz.zlatanov.frakkintoasters.event.player.LaunchViperEvent;
 
-import static xyz.zlatanov.frakkintoasters.event.Followup.all;
+import static xyz.zlatanov.frakkintoasters.event.Followup.*;
 import static xyz.zlatanov.frakkintoasters.state.board.Location.HANGAR_DECK;
+import static xyz.zlatanov.frakkintoasters.state.character.Character.KARA_STARBUCK_THRACE;
 import static xyz.zlatanov.frakkintoasters.state.character.Character.LOUIS_HOSHI;
 
 class HangarDeckEventProcessorTest extends EventTestHarness<HangarDeckEvent> {
 
-    @BeforeEach
-    void setUp() {
-        player(1).character(LOUIS_HOSHI);
-        moveTo(HANGAR_DECK, LOUIS_HOSHI);
-    }
-
     @Test
     void shouldLaunchViperWithExtraAction() {
+        player(1).character(KARA_STARBUCK_THRACE);
+        moveTo(HANGAR_DECK, KARA_STARBUCK_THRACE);
         execute(new HangarDeckEvent(1));
         assertFollowup(
                 all(
-                        new PlayerDecisionEvent<>(1, LaunchViperEvent.class),
-                        new PlayerDecisionEvent<>(1, ActionEvent.class)));
+                        single(
+                                new PlayerDecisionEvent<>(1, LaunchViperEvent.class)),
+                        one(
+                                new PlayerDecisionEvent<>(1, ActionEvent.class),
+                                new NoOpEvent(1))));
+    }
+
+    @Test
+    void shouldNotAllowNonPilotToUseLocation() {
+        player(1).character(LOUIS_HOSHI);
+        moveTo(HANGAR_DECK, LOUIS_HOSHI);
+        assertInvalid(new HangarDeckEvent(1));
     }
 }
