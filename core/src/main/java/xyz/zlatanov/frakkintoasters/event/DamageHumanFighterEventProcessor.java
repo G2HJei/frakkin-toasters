2447ea8646e802ee13f19559910a627b95d47f57
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-09-25 07:47:30 +0100

    Add DamageHumanFighterEventProcessor

--- core/src/main/java/xyz/zlatanov/frakkintoasters/event/DamageHumanFighterEventProcessor.java
+++ core/src/main/java/xyz/zlatanov/frakkintoasters/event/DamageHumanFighterEventProcessor.java
@@ -4,12 +4,19 @@ import lombok.val;
 import xyz.zlatanov.frakkintoasters.EventProcessor;
 import xyz.zlatanov.frakkintoasters.state.ship.HumanFighter;
 
+import static xyz.zlatanov.frakkintoasters.state.board.Location.SICKBAY;
+
 public class DamageHumanFighterEventProcessor extends EventProcessor<DamageHumanFighterEvent> {
 
     @Override
     public Followup process() {
         val ship = galacticaBoard.shipInSpace(event.shipId(), HumanFighter.class);
         galacticaBoard.remove(ship);
+        val pilot = ship.pilot();
+        if (pilot != null) {
+            ship.pilot(null);
+            game.moveTo(SICKBAY, pilot); //todo make this an event to track separately (with label)?
+        }
         galacticaBoard.addToDamagedShips(ship);
         return Followup.NONE;
     }
